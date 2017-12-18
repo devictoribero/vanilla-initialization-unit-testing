266c11e8224f8249b92b13c853c8c76c3e0f042e
Victor Ribero <victor.ribero3@gmail.com>
2017-12-18 21:59:44 +0100

    [NEW] Added bankAccountTest

--- test/BankAccount.js
+++ test/BankAccount.js
@@ -1,10 +1,27 @@
 import {should} from 'chai';
 
 describe('BankAccount --', () => {
-  it('Should have an ID defined');
-  it('Should have an ID auto generated type::string');
-  it('Should have an ID with more than 7 characters');
-  it('Should have a balance defined');
-  it('Shouldn have an balance type::int');
-  it('Shouldn\'t have a negative balance');
+  it('Should have an ID defined', () => {
+    should.exist(id);
+  });
+
+  it('Should have an ID auto generated type::string', () => {
+    id.should.be.a('string');
+  });
+
+  it('Should have an ID with more than 7 characters', () => {
+    id.should.have.lengthOf(7);
+  });
+
+  it('Should have a balance defined', () => {
+    should.exist(balance);
+  });
+
+  it('Shouldn have an balance type::int', () => {
+    balance.should.be.a('string');
+  });
+
+  it('Shouldn\'t have a negative balance', () => {
+  });
+
 });
