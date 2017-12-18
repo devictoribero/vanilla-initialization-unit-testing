8f2b8168f8ed7b7b301f0ec6425c407ba9e92b4d
Victor Ribero <victor.ribero3@gmail.com>
2017-12-18 21:14:05 +0100

    [NEW] Adding requisits for the new test

---
+++ test/BankAccount.js
@@ -0,0 +1,10 @@
+import {should} from 'chai';
+
+describe('BankAccount --', () => {
+  it('Should have an ID defined');
+  it('Should have an ID auto generated type::string');
+  it('Should have an ID with more than 7 characters');
+  it('Should have a balance defined');
+  it('Shouldn have an balance type::int');
+  it('Shouldn\'t have a negative balance');
+});
